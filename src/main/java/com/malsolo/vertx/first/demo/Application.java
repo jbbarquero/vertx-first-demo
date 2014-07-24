eaee403ec0002f83a1ebdae5d53653520ef4e0b5
Javier Beneito Barquero <jbbarquero@gmail.com>
2014-07-24 09:47:43 +0200

    VERTX-FIRST-DEMO-0002: accesing mongoDB, now reading configuration file (-conf) :)

--- src/main/java/com/malsolo/vertx/first/demo/Application.java
+++ src/main/java/com/malsolo/vertx/first/demo/Application.java
@@ -2,6 +2,7 @@ package com.malsolo.vertx.first.demo;
 
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.AsyncResultHandler;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
 /**
@@ -12,12 +13,13 @@ public class Application extends Verticle {
     @Override
     public void start() {
         super.start();
-        container.logger().debug("Application: Deploying PingVerticle...");
-        container.deployVerticle("com.malsolo.vertx.first.demo.PingVerticle");
+        JsonObject appConfig = container.config();
+        container.logger().info("Application: Deploying PingVerticle...");
+        container.deployVerticle("com.malsolo.vertx.first.demo.PingVerticle", appConfig);
         container.logger().info("Application: PingVerticle deployed");
-        container.logger().debug("Application: Deploying Server...");
+        container.logger().info("Application: Deploying Server...");
         //container.deployVerticle("com.malsolo.vertx.first.demo.Server");
-        container.deployVerticle("com.malsolo.vertx.first.demo.Server", new AsyncResultHandler<String>() {
+        container.deployVerticle("com.malsolo.vertx.first.demo.Server", appConfig, new AsyncResultHandler<String>() {
             @Override
             public void handle(AsyncResult<String> deployResult) {
                 if (deployResult.succeeded()) {
